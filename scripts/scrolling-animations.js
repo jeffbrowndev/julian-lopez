720e1c67083f0596c3ef66d7863dbb37ee2066ae
Jeff Brown <jeffbrowndev@gmail.com>
2018-04-16 22:10:39 -0700

    large update to navbar and hover effects

--- scripts/scrolling-animations.js
+++ scripts/scrolling-animations.js
@@ -11,3 +11,21 @@ window.addEventListener('scroll', (e) => {
   const scrolled = -e.pageY * 0.25;
   header.setAttribute("style", "transform: translate3d(0px, " + scrolled + "px, 0px)");
 })
+
+const banner = document.querySelector('#header-banner');
+const psudoBanner = document.querySelector('#header-psudo-banner');
+
+//initialize header based on user device
+window.innerWidth <= 960 && psudoBanner.setAttribute('src', 'images/jl-header-mobile.jpg');
+
+//change header ratio on window resize
+window.addEventListener('resize', () => {
+  if(window.innerWidth <= 960) {
+    banner.setAttribute('src', 'images/jl-header-mobile.jpg')
+    psudoBanner.setAttribute('src', 'images/jl-psudo-header-mobile.png')
+  }
+  else {
+    banner.setAttribute('src', 'images/jl-header.jpg')
+    psudoBanner.setAttribute('src', 'images/jl-psudo-header.png')
+  }
+})
