51be6b98fb598fb450aaf11aaee4739df54f8238
Jeff Brown <jeffbrowndev@gmail.com>
2018-05-06 22:11:58 -0700

    NALAC rough draft

--- scripts/modalControls.js
+++ scripts/modalControls.js
@@ -3,6 +3,8 @@ const modals = Array.from(document.querySelectorAll('.modal'));
 
 contentBoxes.forEach((box) => {
   box.addEventListener('click', () => {
+    //scroll modal to top before re-opening
+    $("#animatedModal").scrollTop(0);
     //find current displayed modal
     const currentModal = modals.find((modal) => {
       return modal.style.display === 'block';
@@ -13,6 +15,7 @@ contentBoxes.forEach((box) => {
     //display new modal
     const id = `#${box.getAttribute('id')}-modal`;
     const project = document.querySelector(id);
+
     project.style.display = 'block';
   })
 })
